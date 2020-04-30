9d6ed323fa104cd4d5dcf66a9759b7a442718ed8
Rodrigo <rodrigomaximocano@gmail.com>
2020-04-30 13:40:31 -0300

    Add static image server rote

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import './database';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
